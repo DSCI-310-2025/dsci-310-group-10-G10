c46d508b154e964f41e888eb1dafc871b250c0e7
KevinzjCheng <kevinzjcheng@gmail.com>
2025-03-15 14:39:50 -0700

    Updated Makefile, added preprocessdata.R, trainmodel.R and updated modelanalysis.R

---
+++ scripts/preprocessdata.R
@@ -0,0 +1,27 @@
+# Load required libraries
+library(tidyverse)
+library(caret)
+library(docopt)
+
+# Define command-line arguments
+'Usage: preprocessdata.R --input_path=<input> --train_path=<train> --test_path=<test> --preprocessor_path=<preprocessor>' -> doc
+args <- docopt(doc)
+
+# Load cleaned data
+airbnb <- read_csv(args$input_path, show_col_types = FALSE)
+
+# Split data into 80% train, 20% test
+set.seed(123)
+train_indices <- createDataPartition(airbnb$realSum, p = 0.8, list = FALSE)
+train_data <- airbnb[train_indices, ]
+test_data <- airbnb[-train_indices, ]
+
+# Save train and test datasets
+write_csv(train_data, args$train_path)
+write_csv(test_data, args$test_path)
+
+# Save preprocessor (if transformations are applied in the future)
+preprocessor <- list(scaling = FALSE, encoding = FALSE)  # Placeholder for transformations
+saveRDS(preprocessor, args$preprocessor_path)
+
+print("Preprocessing completed. Train and test data saved.")
