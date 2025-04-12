99d1d1c6d7600d452cc3cf160dc848f625ae428e
dscidyy <106622265+dscidyy@users.noreply.github.com>
2025-04-12 09:29:48 +0800

    Update split_train_test.R
    adding examples

--- R/split_train_test.R
+++ R/split_train_test.R
@@ -10,6 +10,16 @@
 #'
 #' @return A list containing two elements: 'train' and 'test', which are data frames
 #' @export
+#'
+#' @examples
+#' df <- data.frame(
+#'   x = rnorm(100),
+#'   y = rnorm(100)
+#' )
+#' split_data <- split_train_test(df, train_frac = 0.75)
+#' str(split_data$train)
+#' str(split_data$test)
+
 split_train_test <- function(data, response_col, train_ratio = 0.8, seed = 123) {
   set.seed(seed)
   train_indices <- caret::createDataPartition(data[[response_col]], p = train_ratio, list = FALSE)
