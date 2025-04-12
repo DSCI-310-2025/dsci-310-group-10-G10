7c632a73df30c1a6da57f560ba4bb5a21752ff5d
dscidyy <106622265+dscidyy@users.noreply.github.com>
2025-04-12 09:30:58 +0800

    Update train_model.R
    adding examples

--- R/train_model.R
+++ R/train_model.R
@@ -3,6 +3,19 @@
 #' @param train_data A data frame containing the training set. Must include a column called `price`.
 #' @return A trained caret model.
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(caret)
+#' df <- data.frame(
+#'   price = rnorm(100, mean = 200, sd = 50),
+#'   size = rnorm(100, mean = 80, sd = 10),
+#'   rating = rnorm(100, mean = 4, sd = 0.5)
+#' )
+#' model <- train_model(df)
+#' summary(model)
+#' }
+
 train_model <- function(train_data) {
   library(caret)
 
